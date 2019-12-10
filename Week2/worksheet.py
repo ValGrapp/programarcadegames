99c7115391b630d11f9e8d8f9be72549d8b85ada
Valerie Grappendorf <mac@Valeries-Macbook.local>
2019-12-10 15:53:12 +0100

    week 2

---
+++ Week2/worksheet.py
@@ -0,0 +1,65 @@
+# Give an example of a binary number. (While a number such as ``1'' can be a binary, decimal, and hexadecimal number,
+# try coming up with an example that better illustrates the differences between the different bases of numbers.)
+# Solution: 1010 0010 0011 --> only 0 and 1s
+
+
+# Give an example of a decimal number.
+# Solution: 18 --> numbers 0 to 9
+
+
+# Give an example of a hexadecimal number.
+# Solution: 8F --> 0-9 and A-F
+
+
+# Convert the numbers 1, 10, 100, 1000, and 10000 from binary to decimal.
+# Solution:           1,  2,   4,    8,        16
+
+
+# What is a compiler?
+# Solution: Converts source code to machine code
+
+
+# What is source code?
+# Solution: The program the developer types into the computer
+
+
+# What is machine language? (Don't just say binary. That's not correct.)
+# Solution: The native code the computer runs
+
+
+# What is a first generation language? (Don't just say binary. That's not correct.)
+# Solution: Machine language
+
+
+# What is a second generation language?
+# Solution: Assembly language
+
+
+# What is a third generation language? (Explain, don't just give one example.)
+# Solution: Language like Python or C that has logical structures and also for specific purposes
+
+
+# What is an interpreter and how does it differ from a compiler?
+# Solution: Runs source code directly, without compiling
+
+
+# Search the web and find some of the most popular programming languages.
+# List the website(s) you got the information from and what the languages are.
+# Solution: JavaScript, Python, Java, C++, Swift, TypeScript, Go, SQL, Ruby, R
+# https://www.ubuntupit.com/top-20-most-popular-programming-languages-to-learn-for-your-open-source-project/
+
+
+# Look at the job boards and see what languages people are looking for.
+# List the languages and the job board you looked at.
+# Solution: Python, Java, Javascript (Frontend)
+
+
+# What is the difference between the ``syntax'' and ``semantics'' of a language?
+# Solution: Semantics follow directly from syntax. Syntax refers to the structure/form of
+# the code that a specific programming language specifies but Semantics deal with the meaning
+# assigned to the symbols, characters and words.
+
+
+# Pick a piece of technology, other than a computer you use regularly.
+# Briefly describe the hardware and software that run on it.
+# Solution: iPhone: iOS made with Swift
